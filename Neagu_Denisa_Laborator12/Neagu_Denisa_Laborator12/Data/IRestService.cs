cadb018f97a4b20f5df2b25489bdd34c75c15b89
Denisa Neagu <neagudenisa140@gmail.com>
2021-01-06 12:37:46 +0200

    Add project files.

---
+++ Neagu_Denisa_Laborator12/Neagu_Denisa_Laborator12/Data/IRestService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Neagu_Denisa_Laborator12.Models;
+
+namespace Neagu_Denisa_Laborator12.Data
+{
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+    }
+}
---
+++ Neagu_Denisa_Laborator12/Neagu_Denisa_Laborator12/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Neagu_Denisa_Laborator12
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
